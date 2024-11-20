a697adf77a70e834595cb7222339db3a6490163d
Sivokon Sava <savakapro@gmail.com>
2024-11-20 15:06:54 +0300

    Food Update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import keyboard
 import time
 import colorama
+import random
 
 print(colorama.Fore.GREEN +"\tSnakeInTerminal!\n" + colorama.Style.RESET_ALL + colorama.Fore.CYAN + """
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -21,12 +22,13 @@ print(colorama.Fore.GREEN +"\tSnakeInTerminal!\n" + colorama.Style.RESET_ALL + c
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀""" + colorama.Style.RESET_ALL)
 
 time.sleep(2)
+weight, height = 25, 15  # Длина должна быть больше чем ширина
 
+food = [random.randint(0, weight - 1), random.randint(0, height - 1)]
 head = [0, 5]
 body = [[0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
 max_snake = len(body)
 
-weight, height = 25, 15  # Длина должна быть больше чем ширина
 def draw():
     os.system("cls" if os.name == "nt" else "clear")
     print(("0" * weight) + ("0"))
@@ -37,6 +39,8 @@ def draw():
                line += colorama.Back.BLACK + " " + colorama.Style.RESET_ALL
            elif [w, h] in body:
                line += colorama.Back.BLACK + " " + colorama.Style.RESET_ALL
+           elif [w, h] == food:
+               line += colorama.Back.RED + " " + colorama.Style.RESET_ALL
            else:
                line += colorama.Back.GREEN + " "
         print(line)
@@ -44,28 +48,29 @@ def draw():
 while True:
     draw()
     print(("0" * weight) + ("0"))
+    body[0] = head.copy()
+    
+    for i in reversed(range(max_snake)):  
+            body[i] = body[i - 1]
+
     if keyboard.is_pressed("w") and head[1] > 0:
-        body[0] = head.copy()
         head[1] -= 1
-        for i in reversed(range(max_snake)):
-            body[i] = body[i - 1]
     elif keyboard.is_pressed("s") and head[1] < height - 1:
-        body[0] = head.copy()
         head[1] += 1
-        for i in reversed(range(max_snake)):  
-            body[i] = body[i - 1]
     elif keyboard.is_pressed("a") and head[0] > 0:
-        body[0] = head.copy()
         head[0] -= 1
-        for i in reversed(range(max_snake)):  
-            body[i] = body[i - 1]
     elif keyboard.is_pressed("d") and head[0] < weight - 1:
-        body[0] = head.copy()
         head[0] += 1
-        for i in reversed(range(max_snake)):  
-            body[i] = body[i - 1]
 
     if len(body) > max_snake:
         del body[-1]
+
+    for b in body:
+        if (head[0] == food[0] and head[1] == food[1]):
+            food = [random.randint(0, weight - 1), random.randint(0, height - 1)]
+            max_snake += 1
+            body.insert(0, head.copy())
+
+            break
 
     time.sleep(0.1)
