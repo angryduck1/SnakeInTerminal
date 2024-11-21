05ab7fa218cb8a31657fb798f593a0f42b7688be
Sivokon Sava <savakapro@gmail.com>
2024-11-21 16:26:42 +0300

    Border Update

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ food = [random.randint(0, weight - 1), random.randint(0, height - 1)]
 head = [0, 5]
 body = [[0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
 max_snake = len(body)
+input_controller = ["s"]
 
 def draw():
     os.system("cls" if os.name == "nt" else "clear")
@@ -42,7 +43,7 @@ def draw():
            elif [w, h] == food:
                line += colorama.Back.RED + " " + colorama.Style.RESET_ALL
            else:
-               line += colorama.Back.GREEN + " "
+               line += colorama.Back.LIGHTGREEN_EX + " "
         print(line)
 
 while True:
@@ -53,24 +54,51 @@ while True:
     for i in reversed(range(max_snake)):  
             body[i] = body[i - 1]
 
-    if keyboard.is_pressed("w") and head[1] > 0:
+    if keyboard.is_pressed("w"):
         head[1] -= 1
-    elif keyboard.is_pressed("s") and head[1] < height - 1:
+        input_controller.append("w")
+    elif keyboard.is_pressed("s"):
         head[1] += 1
-    elif keyboard.is_pressed("a") and head[0] > 0:
+        input_controller.append("s")
+    elif keyboard.is_pressed("a"):
         head[0] -= 1
-    elif keyboard.is_pressed("d") and head[0] < weight - 1:
+        input_controller.append("a")
+    elif keyboard.is_pressed("d"):
         head[0] += 1
+        input_controller.append("d")
+    else:
+        if input_controller[-1] == "w":
+            head[1] -= 1
+            input_controller.append("w")
+        elif input_controller[-1] == "s":
+            head[1] += 1
+            input_controller.append("s")
+        elif input_controller[-1] == "a":
+            head[0] -= 1
+            input_controller.append("a")
+        elif input_controller[-1] == "d":
+            head[0] += 1
+            input_controller.append("d")
 
-    if len(body) > max_snake:
-        del body[-1]
+        if body[0][0] < 0 or body[0][0] >= weight or body[0][1] < 0 or body[0][1] >= height:
+            food = [random.randint(0, weight - 1), random.randint(0, height - 1)]
+            head = [0, 5]
+            body = [[0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
+            max_snake = len(body)
+            input_controller = ["s"]
+        else:
+            pass
+
 
     for b in body:
         if (head[0] == food[0] and head[1] == food[1]):
             food = [random.randint(0, weight - 1), random.randint(0, height - 1)]
             max_snake += 1
-            body.insert(0, head.copy())
+            body.append(head.copy())
 
             break
+
+    if len(body) > max_snake:
+        del body[-1]
 
     time.sleep(0.1)
