d04dfe874cbd1da4f0ac062894d6f81f61b45970
Sivokon Sava <savakapro@gmail.com>
2024-11-21 17:44:42 +0300

    Pre Finally Update

--- main.py
+++ main.py
@@ -89,6 +89,15 @@ while True:
         else:
             pass
 
+        if head in body:
+            food = [random.randint(0, weight - 1), random.randint(0, height - 1)]
+            head = [0, 5]
+            body = [[0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]
+            max_snake = len(body)
+            input_controller = ["s"]
+        else:
+            pass
+
 
     for b in body:
         if (head[0] == food[0] and head[1] == food[1]):
